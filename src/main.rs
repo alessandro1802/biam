a8ccc2a7625e44a8d9b15e5b8ee50e4b4815a0a1
Alessandro <oleksandr.yasinskyi@icloud.com>
2024-03-06 18:29:27 +0100

    added a function to generate random permutation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::prelude::*;
+
+fn random_permutation(n: usize) -> Vec<usize> {
+    let mut permutation: Vec<usize> = (0..n).collect();
+    let mut rng = rand::thread_rng();
+
+    for i in (1..n).rev() {
+        let j = rng.gen_range(0..=i);
+        permutation.swap(i, j);
+    }
+
+    permutation
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 10;
+    let permutation = random_permutation(n);
+    println!("Random permutation: {:?}", permutation);
 }
