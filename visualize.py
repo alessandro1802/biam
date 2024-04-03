3e00928ba14c255ab020cfd029da02092218a0c1
Alessandro <oleksandr.yasinskyi@icloud.com>
2024-04-03 20:19:06 +0200

    #6 added Efficiency plot

--- visualize.py
+++ visualize.py
@@ -16,7 +16,7 @@ def read_results(data_path):
                 results[instance_name][algorithm_name] = json.load(file)
     return results
 
-def efficiency_plot(algorithm_order, save_path):
+def quality_plot(algorithm_order, save_path):
     rows = 2
     cols = int(len(results) / 2)
     bar_width = 1
@@ -73,6 +73,38 @@ def runtime_plot(algorithm_order, save_path):
     fig.suptitle("Runtime", fontsize = 32)
     plt.savefig(save_path)
 
+def efficiency_plot(algorithm_order, save_path, weight_runtime = 0.5, weight_score = 0.5):
+    def normilize(array):
+        return (array - array.min()) / (array.max() - array.min())
+
+    rows = 2
+    cols = int(len(results) / 2)
+    bar_width = 1
+
+    fig, axes = plt.subplots(rows, cols, figsize = (16, 9))
+    fig.tight_layout(h_pad=5)
+    plt.subplots_adjust(top=0.9)
+
+    for i, (instance_name, algrorithms) in enumerate(results.items()):
+        row = i // 4
+        col = i - row * 4
+
+        pos = np.arange(len(algorithm_order)) * bar_width
+
+        score_avg = normilize(np.array([np.mean(algrorithms[algorithm_name]["distances"]) for algorithm_name in algorithm_order]))
+        runtimes_avg = normilize(np.array([np.mean(algrorithms[algorithm_name]["runtimes"]) for algorithm_name in algorithm_order]))
+        efficiency_scores = 1 - ((weight_runtime * runtimes_avg) + (weight_score * score_avg))
+
+        axes[row, col].bar(pos, efficiency_scores, edgecolor='white', width=bar_width)
+
+        axes[row, col].set_title(instance_name)
+        axes[row, col].set_xticks(pos)
+        axes[row, col].set_xticklabels(algorithm_order, fontsize=7)
+        axes[row, col].tick_params(axis='y', labelsize=7)
+
+    fig.suptitle(f"Efficiency with Runtime weight = {weight_runtime} and Score weight = {weight_score}", fontsize = 32)
+    plt.savefig(save_path)
+
 
 data_path = "./results"
 optima = {"berlin52": 7_542,
@@ -92,11 +124,15 @@ if __name__ == "__main__":
     
     # Quality plots
     save_path = './plots/quality_all.svg'
-    efficiency_plot(algorithm_order, save_path)
+    quality_plot(algorithm_order, save_path)
     
     save_path = './plots/quality_no-R.svg'
-    efficiency_plot(algorithm_order[:-1], save_path)
+    quality_plot(algorithm_order[:-1], save_path)
     
     # Runtime plot
     save_path = './plots/runtime.svg'
     runtime_plot(algorithm_order, save_path)
+
+    # Efficiency plot
+    save_path = './plots/efficiency.svg'
+    efficiency_plot(algorithm_order, save_path)
