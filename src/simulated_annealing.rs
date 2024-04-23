692cdcc911d31626d7d89ad5e151c48dd7e0895f
Alessandro <oleksandr.yasinskyi@icloud.com>
2024-04-23 17:44:50 +0200

    updated SA

--- src/simulated_annealing.rs
+++ src/simulated_annealing.rs
@@ -28,7 +28,7 @@ impl SimulatedAnnealing {
     pub fn new(distance_matrix: Vec<Vec<f32>>) -> SimulatedAnnealing {
         let n = distance_matrix.len();
         let T = 0.0;
-        // Exponential decay
+
         let alpha = 0.99;
         SimulatedAnnealing {
             distance_matrix,
@@ -37,19 +37,19 @@ impl SimulatedAnnealing {
             alpha,
         }
     }
-    
+
     /**
     * Determine initial temperature.
     */ 
     pub fn determine_initial_temperature(&mut self) {
         let current_tour = utils::random_permutation(self.n);
-        
+
         let mut rng = rand::thread_rng();
         let sample_n = rng.gen_range(0..self.n / 2);
-        
+
         let mut n_samples = 0;
-        let mut average_delta: f64 = 0.0;
-        
+        let mut avg_pos_delta: f64 = 0.0;
+
         // Get a sample of edges
         for i in 0..sample_n {
             let next_i = i + 1;
@@ -59,16 +59,16 @@ impl SimulatedAnnealing {
                 if next_j == i { continue; }
                 // Calculated delta fitness
                 let delta = utils::get_delta_intra_route(&self.distance_matrix, current_tour[i], current_tour[next_i], current_tour[j], current_tour[next_j]);
-                if delta < 0.0 {
-                    average_delta += delta as f64;
+                if delta > 0.0 {
+                    avg_pos_delta += delta as f64;
+                    n_samples += 1;
                 }
-                n_samples += 1;
             }
         }
-        average_delta /= n_samples as f64;
+        avg_pos_delta /= n_samples as f64;
         
-        // Set acceptance probability to 98/100;
-        self.T = -100.0 * average_delta / 98.0;
+        // Set acceptance probability of non-improving solution to 95%;
+        self.T = -avg_pos_delta / (0.99_f64).ln();
     }
 
     
@@ -84,11 +84,10 @@ impl SimulatedAnnealing {
 
         let mut evaluated = 0;
         let mut steps = 0;
+//        let mut no_improvement = 0;
 
-        let mut no_improvement = 0;
-        let mut accept = true;
-        while current_T > 1.0 {
-            accept = false;
+        while current_T > 0.001 {
+            let mut accept = false;
             // Intra-route neighbourhood: Iterate all distinct 2-edge pairs
             for i in 0..self.n {
                 let next_i = (i + 1) % self.n;
@@ -100,7 +99,7 @@ impl SimulatedAnnealing {
                     let delta = utils::get_delta_intra_route(&self.distance_matrix,current_tour[i], current_tour[next_i], current_tour[j], current_tour[next_j]);
                     evaluated += 1;
 
-                    if delta < 0.0 || -delta as f64 / current_T > rand::random() {
+                    if delta < 0.0 || f64::exp(-delta as f64 / current_T) > rand::random() {
                         best_tour = utils::swap_2_edges(&current_tour, next_i, j, best_tour);
                         accept = true;
                         break;
@@ -108,19 +107,23 @@ impl SimulatedAnnealing {
                 }
                 if accept {
                     current_tour = best_tour.clone();
-                    no_improvement = 0;
+//                    no_improvement = 0;
                     steps += 1;
                     break;
                 }
             }
-            if !accept {
-                no_improvement += 1;
-                if no_improvement >= 10 {
-                    break;
-                }
-            }
+//            if !accept {
+//                no_improvement += 1;
+//                // Stopping criterion: no improvement
+//                if no_improvement >= 10 {
+//                    break;
+//                }
+//            }
             // Decrease the temperature T by multiplying with the cooling rate alpha
-            current_T *= self.alpha;
+            // Geometric decay
+//            current_T *= self.alpha;
+            // Exponential decay
+            current_T = current_T / (1.0 + self.alpha * current_T);
         }
         
         let distance = utils::calculate_tour_distance(&current_tour, &self.distance_matrix).unwrap();
